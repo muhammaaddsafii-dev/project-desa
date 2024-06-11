7ba8c3ef583bcea2c98502c20ca6b1a581991c8a
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2024-06-11 09:52:54 +0700

    language/update-language-into-bahasa

---
+++ resources/lang/id/filament.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'actions' => [
+        'create' => 'Buat',
+        'delete' => 'Hapus',
+        'edit' => 'Edit',
+        'view' => 'Lihat',
+        'save' => 'Simpan',
+        'cancel' => 'Batal',
+    ],
+    'fields' => [
+        'name' => 'Nama',
+        'email' => 'Email',
+        'password' => 'Kata Sandi',
+    ],
+    'messages' => [
+        'created' => 'Data berhasil dibuat.',
+        'deleted' => 'Data berhasil dihapus.',
+        'updated' => 'Data berhasil diperbarui.',
+    ],
+    'navigation' => [
+        'dashboard' => 'Dasbor',
+        'users' => 'Pengguna',
+        'settings' => 'Pengaturan',
+        'list' => 'Daftar',
+    ],
+];
