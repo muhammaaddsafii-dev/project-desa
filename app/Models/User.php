f7345ff6feb4c47fbd221933941bf3d35b093083
muhammaaddsafii-dev <muhammaaddsafii@gmail.com>
2024-06-11 07:23:53 +0700

    database/update-struktur-table

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
+    }
+
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
 }
